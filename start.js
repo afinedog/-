2a8461044382ab25841918776841c9e8146aeb04
afinedog <1045654388@qq.com>
2023-12-19 16:00:20 +0800

    解决了对话框异步问题

--- start.js
+++ start.js
@@ -1,15 +1,3 @@
-/* 查看荣誉 */
-// 荣誉页面出现函数
-function honor_show()
-{
-
-}
-// 点击查看荣誉按钮
-$("#honor").click(function (){ 
-    // 主页面隐身
-    rule_vanish(".start-page"); rule_vanish(".rule-intro");
-    honor_show();
- });
 
 /* 游戏规则 */
 // 规则说明页面出现函数
@@ -72,6 +60,15 @@ $("#third").click(function () {
     rule_show(".start-page");
 })
 
+/* 查看荣誉 */
+// 点击查看荣誉按钮
+$("#honor").click(function (){ 
+    // 主页面隐身
+    rule_vanish(".start-page"); rule_vanish(".rule-intro");
+    rule_show("#honor-intro")
+ });
+ // 单击荣誉界面
+
 /* 开始游戏 */
 // 点击开始按钮
 $("#game-start").click(function () { 
@@ -89,19 +86,25 @@ $("#sex").click(function(){
 
 // 单击提交时
 $("#submit").click(function () { 
-    // 检查姓名和性别有无填写，若没有则显示提示语句
+    // 检查姓名和性别有无填写，若没有则显示提示语句并退出函数
     if ($("input[type='text']").value == ' ' && $("#sex").value == 'off')
-        { $("#tip2").css("opacity",1);    $("#tip2").css("opacity",0);    }
+        { $("#tip2").css("opacity",1);    $("#tip1").css("opacity",0);  return;  }
     else
     {
         // 检查复选框是否全部填写
         $("input[type='checkbox']").each(function () { 
-            // 若没有全部填写，显示提示语句
+            // 若没有全部填写，显示提示语句并退出函数
             if (this.checked == false)
             {  $("#tip1").css("opacity",1);  return; }
             // 若全部填写，则记录值并进入下一环节
             else
-            {  you = {}  }
+            {   
+                // 初始化身份
+                identity();
+                // 根据专业分配荣誉
+                honor_check();
+                window.location.replace("./dormitory.html");  //跳转到寝室页面
+            }
         })
     }
 });
@@ -110,28 +113,98 @@ $("#submit").click(function () {
 class student
 {
     // 构造函数
-    student(name,num,life,act,def,hp,dad){
+    student(    // 参数列表
+                name,
+                sex ,
+                major,
+                num = `${202100091100 + Math.round(100*Math.random())}`,  // 用反引号生成随机数数字
+                life = 5,       // 体力
+                act = 5, def = 5, hp = 5, // 攻击、防御、生命值
+                day = 0,
+                dad = [''],     // 干爹名单)
+                honor = ['']    // 已获得的荣誉
+            )
+    // 作弊模式专用
+    {
         this.name = name;       // 名字
-        this.number = num;
+        this.sex = sex;         // 性别
+        this.major = major;     // 专业
+        this.number = num;      // 学号
         this.life = life;       // 体力
         this.act = act;  this.def = def;  this.hp = hp; // 攻击、防御、生命值
+        this.day = day;     // 经过天数
         this.dad = dad ;    // 干爹名单
+        this.honor = honor;
     }
-    name = '李鼠';  // 默认名字
-    number = `${202100091100 + Math.round(100*Math.random())}`;  // 用反引号生成随机数数字
-    life = 5;       // 体力
-    act = 5; def = 5; hp = 5; // 攻击、防御、生命值
-    dad = [''] ;    // 干爹名单
+
 }
 
 // 初始化玩家身份
 function identity(){
-    you.name = $("paper")
+    // （非作弊模式）
+    you = new student(
+        $("input[name='姓名']").value,
+        $("#sex")[0].value + $("#sex")[1].value,
+        $("select")[0].value
+        );
+    // （作弊模式，给出一张表单，自己填写数值）
 }
+
+// 检测荣誉
+function honor_check() { 
+    switch (you.major) {
+        case '数学专业': 
+        case '物理专业': 
+        case '计算机专业': 
+        case '土木专业': 
+        case '医学专业': 
+        case '电气电子专业':
+        case '传统工科': 
+        case '生化环材地': 
+        case '金融专业': 
+        case '文史哲法': 
+    }
+    
+ }
+
+ /* 通知和判断图窗 */
+// 普通的消失函数
+function vanish(ele){ $(ele).css({"opacity": "0", "z-index":"-1"}); }
+function show(ele) { $(ele).css({"opacity": "1", "z-index":"100"}); }
+// 提醒函数，只有接受一个选项
+function inform(){
+    show($("#inform"));
+    // 单击后图窗消失
+    $("#know").click(function(){
+        vanish($("#inform"))
+    });
+}
+
+// 选择函数，有是或否两个选项（异步函数）
+
+function choice() {
+    show("#choice");
+    var choice_res;
+    var deferred = $.Deferred();
+    $('#yes').click(function() {
+        vanish($("#choice"));
+        choice_res = true;
+        deferred.resolve(choice_res);
+    });
+    $('#no').click(function() {
+        vanish($("#choice"));
+        choice_res = false;
+        deferred.resolve(choice_res);
+    });
+    return deferred.promise();
+  }
+  
+// 若要使a为调用的值，只需要：
+//  choice().done(function(choice_res) { a = choice_res; })
 
 /* 新手教程 */
 $("#guide").click(function () { 
-    window.location.replace("./dormitory.html")
+    
 })
 
 
